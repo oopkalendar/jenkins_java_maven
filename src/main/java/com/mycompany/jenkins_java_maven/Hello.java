5f3a3d3c857958c5e2363ad995afde8a9a70f6aa
oopkalendar <oopkalendar@gmail.com>
2017-06-01 12:50:13 +0200

    initialization

---
+++ src/main/java/com/mycompany/jenkins_java_maven/Hello.java
@@ -0,0 +1,7 @@
+package com.mycompany.jenkins_java_maven;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello from jenkins java maven project");
+    }
+}
