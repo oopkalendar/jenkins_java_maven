17a63e2883c90ecaf558f7e7a7daf92480b9ae38
oopkalendar <oopkalendar@gmail.com>
2017-06-01 15:13:20 +0200

    added Calculator with uni tests

---
+++ src/main/java/com/mycompany/jenkins_java_maven/Calculator.java
@@ -0,0 +1,7 @@
+package com.mycompany.jenkins_java_maven;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/mycompany/jenkins_java_maven/CalculatorTest.java
@@ -0,0 +1,55 @@
+package com.mycompany.jenkins_java_maven;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sum method, of class Calculator.
+     */
+    @org.junit.Test
+    public void testSumPositive() {
+        System.out.println("sum");
+        int a = 1;
+        int b = 2;
+        Calculator instance = new Calculator();
+        int expResult = 3;
+        int result = instance.sum(a, b);
+        assertEquals(expResult, result);
+    }
+
+    @org.junit.Test
+    public void testSumZero() {
+        System.out.println("sum");
+        int a = 0;
+        int b = 0;
+        Calculator instance = new Calculator();
+        int expResult = 0;
+        int result = instance.sum(a, b);
+        assertEquals(expResult, result);
+    }
+}
